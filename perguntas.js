f319514d558482330459f1f372741ead888d3c7b
Victoria Dittrich <victoria.dittrich@escola.pr.gov.br>
2025-06-08 21:06:09 -0300

    Usando o js

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Curiosidades1',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades2',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades3',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades4',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades5',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades6',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades7',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades8',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades9',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades10',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades11',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Curiosidades12',
+    pergunta,
+    resposta
+)
