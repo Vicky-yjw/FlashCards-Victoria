464a37f548e90d69c04117d546d6f9590c384e3f
Victoria Dittrich <victoria.dittrich@escola.pr.gov.br>
2025-06-09 16:28:31 -0300

    Alteração no perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -12,60 +12,60 @@ criaCartao(
 
 criaCartao(
     'Curiosidades3',
-    pergunta,
-    resposta
+    pergunta = 'Quem propôs a Teoria atômica?',
+    resposta = 'A Teoria atômica foi proposta por John Dalton em 1803. Ele sugeriu que a matéria é composta por átomos indivisíveis.'
 )
 
 criaCartao(
     'Curiosidades4',
-    pergunta,
-    resposta
+    pergunta = 'Quem propôs a Teoria da Relatividade?',
+    resposta = 'A Teoria da Relatividade foi proposta por Albert Einstein em 1905 e 1915.'
 )
 
 criaCartao(
     'Curiosidades5',
-    pergunta,
-    resposta
+    pergunta = 'Qual o idioma mais falado no mundo?',
+    resposta = 'O mandarim, com mais de 1 bilhão de falantes nativos.'
 )
 
 criaCartao(
     'Curiosidades6',
-    pergunta,
-    resposta
+    pergunta = 'Qual o país com mais ilhas do mundo?',
+    resposta = 'A Suécia.'
 )
 
 criaCartao(
     'Curiosidades7',
-    pergunta,
-    resposta
+    pergunta = 'Qual país tem o maior número de vulcões?',
+    resposta = 'A Indonésia, com mais de 130 vulcões ativos.'
 )
 
 criaCartao(
     'Curiosidades8',
-    pergunta,
-    resposta
+    pergunta = 'Quem é o compositor da - "As quatro estações"?',
+    resposta = 'Antonio Vivaldi.'
 )
 
 criaCartao(
     'Curiosidades9',
-    pergunta,
-    resposta
+    pergunta = 'Quem pintou o quadro - "Lírios de água"?',
+    resposta = 'Claude Monet.'
 )
 
 criaCartao(
     'Curiosidades10',
-    pergunta,
-    resposta
+    pergunta = 'Quantos tipos de bioma existem no Brasil?',
+    resposta= 'No Brasil possui 6 biomas terrestres principais e 1 bioma marinho, totalizando 7 biomas.'
 )
 
 criaCartao(
     'Curiosidades11',
-    pergunta,
-    resposta
+    pergunta = 'Qual a maior palavra registrada na língua portuguesa?',
+    resposta = 'Pneumoultramicroscopicsilicovulcanoconiótico, refere-se a uma doença pulmonar causada pela inalação de partículas muito finas de sílica vulcânica.'
 )
 
 criaCartao(
     'Curiosidades12',
-    pergunta,
-    resposta
+    pergunta = 'Qual foi a maior civilização da Antiguidade?',
+    resposta = 'O Império Romano, que dominou grande parte da Europa, Norte da África e partes da Ásia por mais de 500 anos.'
 )
