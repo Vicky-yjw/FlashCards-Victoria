fbb8566066cfc23fed88591d884d591698ee15f4
Victoria Dittrich <victoria.dittrich@escola.pr.gov.br>
2025-06-09 17:13:48 -0300

    Comentando o index.html

--- perguntas.js
+++ perguntas.js
@@ -1,71 +1,71 @@
 criaCartao(
     'Curiosidades1',
-    pergunta = 'Quantos tipos de sangue existem?',
-    resposta = 'Existem quatro tipos de sangue: A, B, AB e O. Cada tipo pode ser positivo ou negativo, totalizando oito tipos de sangue diferentes.'
+    'Quantos tipos de sangue existem?',
+    'Existem quatro tipos de sangue: A, B, AB e O. Cada tipo pode ser positivo ou negativo, totalizando oito tipos de sangue diferentes.'
 )
 
 criaCartao(
     'Curiosidades2',
-    pergunta = 'Qual o maior animal terrestre?',
-    resposta = 'O maior animal terrestre é o elefante africano, que pode pesar até 6 toneladas e medir até 4 metros de altura.'
+    'Qual o maior animal terrestre?',
+    'O maior animal terrestre é o elefante africano, que pode pesar até 6 toneladas e medir até 4 metros de altura.'
 )
 
 criaCartao(
     'Curiosidades3',
-    pergunta = 'Quem propôs a Teoria atômica?',
-    resposta = 'A Teoria atômica foi proposta por John Dalton em 1803. Ele sugeriu que a matéria é composta por átomos indivisíveis.'
+    'Quem propôs a Teoria atômica?',
+    'A Teoria atômica foi proposta por John Dalton em 1803. Ele sugeriu que a matéria é composta por átomos indivisíveis.'
 )
 
 criaCartao(
     'Curiosidades4',
-    pergunta = 'Quem propôs a Teoria da Relatividade?',
-    resposta = 'A Teoria da Relatividade foi proposta por Albert Einstein em 1905 e 1915.'
+    'Quem propôs a Teoria da Relatividade?',
+    'A Teoria da Relatividade foi proposta por Albert Einstein em 1905 e 1915.'
 )
 
 criaCartao(
     'Curiosidades5',
-    pergunta = 'Qual o idioma mais falado no mundo?',
-    resposta = 'O mandarim, com mais de 1 bilhão de falantes nativos.'
+    'Qual o idioma mais falado no mundo?',
+    'O mandarim, com mais de 1 bilhão de falantes nativos.'
 )
 
 criaCartao(
     'Curiosidades6',
-    pergunta = 'Qual o país com mais ilhas do mundo?',
-    resposta = 'A Suécia.'
+    'Qual o país com mais ilhas do mundo?',
+    'A Suécia.'
 )
 
 criaCartao(
     'Curiosidades7',
-    pergunta = 'Qual país tem o maior número de vulcões?',
-    resposta = 'A Indonésia, com mais de 130 vulcões ativos.'
+    'Qual país tem o maior número de vulcões?',
+    'A Indonésia, com mais de 130 vulcões ativos.'
 )
 
 criaCartao(
     'Curiosidades8',
-    pergunta = 'Quem é o compositor da - "As quatro estações"?',
-    resposta = 'Antonio Vivaldi.'
+    'Quem é o compositor da - "As quatro estações"?',
+    'Antonio Vivaldi.'
 )
 
 criaCartao(
     'Curiosidades9',
-    pergunta = 'Quem pintou o quadro - "Lírios de água"?',
-    resposta = 'Claude Monet.'
+    'Quem pintou o quadro - "Lírios de água"?',
+    'Claude Monet.'
 )
 
 criaCartao(
     'Curiosidades10',
-    pergunta = 'Quantos tipos de bioma existem no Brasil?',
-    resposta= 'No Brasil possui 6 biomas terrestres principais e 1 bioma marinho, totalizando 7 biomas.'
+    'Quantos tipos de bioma existem no Brasil?',
+    'No Brasil possui 6 biomas terrestres principais e 1 bioma marinho, totalizando 7 biomas.'
 )
 
 criaCartao(
     'Curiosidades11',
-    pergunta = 'Qual a maior palavra registrada na língua portuguesa?',
-    resposta = 'Pneumoultramicroscopicsilicovulcanoconiótico, refere-se a uma doença pulmonar causada pela inalação de partículas muito finas de sílica vulcânica.'
+    'Qual a maior palavra registrada na língua portuguesa?',
+    'Pneumoultramicroscopicsilicovulcanoconiótico, refere-se a uma doença pulmonar causada pela inalação de partículas muito finas de sílica vulcânica.'
 )
 
 criaCartao(
     'Curiosidades12',
-    pergunta = 'Qual foi a maior civilização da Antiguidade?',
-    resposta = 'O Império Romano, que dominou grande parte da Europa, Norte da África e partes da Ásia por mais de 500 anos.'
+    'Qual foi a maior civilização da Antiguidade?',
+    'O Império Romano, que dominou grande parte da Europa, Norte da África e partes da Ásia por mais de 500 anos.'
 )
